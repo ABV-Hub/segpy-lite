de091bf9ff3b575d70cd2704a6d104cc0cec44dc
Yu Hao <yuhao89@live.cn>
2017-11-05 20:28:41 +0800

    make setuptools unzip egg file

--- setup.py
+++ setup.py
@@ -1,105 +1,37 @@
-import io
-import os
-import re
+# setup.py
 from distutils.core import setup
-from setuptools import find_packages  # Always prefer setuptools over distutils
-# from codecs import open  # To use a consistent encoding
-from os import path
+from setuptools import find_packages
 
 
-# def read(*names, **kwargs):
-#     with io.open(
-#         os.path.join(os.path.dirname(__file__), *names),
-#         encoding=kwargs.get("encoding", "utf8")
-#     ) as fp:
-#         return fp.read()
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Scientific/Engineering :: Mathematics',
+    'Topic :: Scientific/Engineering :: Physics',
+    'Operating System :: Microsoft :: Windows',
+    'Operating System :: POSIX',
+    'Operating System :: Unix',
+    'Operating System :: MacOS',
+    'Natural Language :: English',
+]
 
-
-# def find_version(*file_paths):
-#     version_file = read(*file_paths)
-#     version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-#                               version_file, re.M)
-#     if version_match:
-#         return version_match.group(1)
-#     raise RuntimeError("Unable to find version string.")
-
-
-# here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the relevant file
-# with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
-#     long_description = f.read()
 
 setup(
     name='segpy-lite',
-    # version=find_version("segpy/__init__.py"),
-    version="1.0.0",
+    version="1.0.1",
     description='Transfer of seismic data to and from SEG Y files',
-    # long_description=long_description,
-
-    # The project's main homepage.
+    long_description="",
     url='https://github.com/whimian/segpy-lite',
-
-    # Author details
     author='Yu Hao',
     author_email='yuhao89@live.cn',
-
-    # Choose your license
     license='MIT',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Software Development :: Libraries',
-
-        # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: GNU Affero General Public License v3',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-    ],
-
-    # What does your project relate to?
+    classifiers=CLASSIFIERS,
     keywords='seismic geocomputing geophysics',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    packages=find_packages(),
-
-    # List run-time dependencies here.  These will be installed by pip when your
-    # project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    # install_requires=[],
-
-    # List additional groups of dependencies here (e.g. development dependencies).
-    # You can install these using the following syntax, for example:
-    # $ pip install -e .[dev,test]
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    # package_data={
-    # },
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages.
-    # see http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    # data_files=[],
+    packages=find_packages(exclude=['test']),
     platforms=["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"],
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
-    # entry_points={
-    #     'console_scripts': [
-    #         'segpy = segpy.cli:main',
-    #     ],
-    # }
+    zip_safe = False
 )
